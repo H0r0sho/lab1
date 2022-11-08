44221dfaf8cd3ba6ef8be69d813ea03df239403d
H0r0sho <undeaddragon163rus@gmail.com>
2022-11-08 13:18:59 +0400

    готовый вариант

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ import requests
 file_name="dataset.csv"
 url = "https://www.cbr-xml-daily.ru/archive/2022/10/22/daily_json.js"
 data = requests.get(url).json()
-print(url)
 date=(data['Date'])
-print(date)
 value=(data['Valute']['USD']['Value'])
-print(value)
 previousurl=(data['PreviousURL'])
-print(previousurl)
 def get_previous_url(url):
     data=requests.get(url).json()
     previousurl=(data['PreviousURL'])
@@ -29,11 +25,8 @@ with open(file_name, "w", encoding="utf-8", newline="") as fh:
     writer = csv.writer(fh, quoting=csv.QUOTE_ALL)
     writer.writerow(["Дата", "Value"])
     while (url!='https://www.cbr-xml-daily.ru/archive/1992/06/30/daily_json.js'):
-        print(url)
         value = get_value(url)
-        print(value)
         date = get_date(url)
-        print(date)
         writer.writerow([f'{date}, {value}'])
         newurl = get_previous_url(url)
         url = newurl
