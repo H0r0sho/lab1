8b4b43ff86c556496790c04ebcb3d10310a12296
H0r0sho <undeaddragon163rus@gmail.com>
2022-11-14 10:17:40 +0400

    правки

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-from pprint import pprint
-from bs4 import BeautifulSoup
 import csv
 import requests
-file_name="dataset.csv"
+file_name = "dataset.csv"
 url = "https://www.cbr-xml-daily.ru/archive/2022/10/22/daily_json.js"
-data = requests.get(url).json()
-date=(data['Date'])
-value=(data['Valute']['USD']['Value'])
-previousurl=(data['PreviousURL'])
+
 def get_previous_url(url):
-    data=requests.get(url).json()
-    previousurl=(data['PreviousURL'])
-    newurl=f'https:{previousurl}'
+    data = requests.get(url).json()
+    previousurl = (data['PreviousURL'])
+    newurl = f'https:{previousurl}'
     return newurl
+
 def get_date(url):
     data = requests.get(url).json()
-    newdate=(data['Date'])
+    newdate = (data['Date'])
     shortdate = newdate[:10]
     return shortdate
+
 def get_value(url):
     data = requests.get(url).json()
     newvalue = (data['Valute']['USD']['Value'])
     return newvalue
-with open(file_name, "w", encoding="utf-8", newline="") as fh:
-    writer = csv.writer(fh, quoting=csv.QUOTE_ALL)
+with open(file_name, "w", encoding = "utf-8", newline = "") as fh:
+    writer = csv.writer(fh, quoting = csv.QUOTE_ALL)
     writer.writerow(["Дата, Курс"])
-    while (url!='https://www.cbr-xml-daily.ru/archive/1992/06/30/daily_json.js'):
+    while (url != 'https://www.cbr-xml-daily.ru/archive/1992/06/30/daily_json.js'):
         value = get_value(url)
         date = get_date(url)
         writer.writerow([f'{date}, {value}'])
